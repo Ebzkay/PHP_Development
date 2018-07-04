0a9469ee8468c6748102cf3d1459052ff7982af6
Ebzkay <40651115+Ebzkay@users.noreply.github.com>
2018-07-04 12:22:13 -0400

    Using Php arrays

---
+++ index1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $myFile = "test.txt";
+        $fh = fopen($myFile, 'r')or die("can't open file");
+        
+        $theData = fread($fh, filesize($myFile));
+        fclose($fh);
+        echo $theData;
+        $arr = ARRAY("damn", "freaking");
+        $changestr = str_replace($arr, '***', $theData);
+        echo "<br>";
+        echo "$changestr";
+
+
+        ?>
+    </body>
+</html>
